f2adc4b5f650bae2ced7b2f6089f33f86e73100a
s1f102****** <s1f102402079@iniad.org>
2025-12-10 11:13:18 +0900

    models.pyにテーブルを作成

--- vs/models.py
+++ vs/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Message(models.Model):
+    username = models.CharField(max_length=30)
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.username}: {self.text[:15]}"
